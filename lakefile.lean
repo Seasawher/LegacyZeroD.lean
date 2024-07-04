d9ceda4b6d4733ed4c240236f4c9f250f032c8a6
Seasawher <tellagoodstory@icloud.com>
2024-07-04 14:42:31 +0900

    sciLean を import する

--- lakefile.lean
+++ lakefile.lean
@@ -8,6 +8,9 @@ package «ZeroD» where
     ⟨`relaxedAutoImplicit, false⟩
   ]
 
+require scilean from git
+  "https://github.com/lecopivo/SciLean.git" @ "master"
+
 @[default_target]
 lean_lib «ZeroD» where
   -- add library configuration options here
